03025797c7b4212279636bad81afd1067f8e3ed2
Wang Yimin <wangyimin@yahoo.com>
2024-04-13 10:23:52 +0900

    CreateFile hooker

--- DetourHook/dllmain.cpp
+++ DetourHook/dllmain.cpp
@@ -1,14 +1,43 @@
 ﻿// dllmain.cpp : DLL アプリケーションのエントリ ポイントを定義します。
 #include "pch.h"
+#include "debugapi.h"
+#include "shlwapi.h"
+#pragma comment(lib, "shlwapi.lib")
+#include "winbase.h"
+#include <tchar.h>
+#include <stdlib.h>
 #include "detours.h"
 
 INT(WINAPI* oMessageBoxW)(HWND, LPCWSTR, LPCWSTR, UINT) = MessageBoxW;
 
-// Define our hook, match return code and parameters
+// Hooker for MessageBox
 int WINAPI hookedMessageBoxW(HWND hWnd, LPCWSTR lpText, LPCWSTR lpCaption, UINT uType)
 {
 	lpText = L"Hooked!";
 	return oMessageBoxW(hWnd, lpText, lpCaption, uType);
+}
+
+// Redirect file location to my document of current user
+void GetRedirectPath(LPCWSTR path, LPWSTR redirect)
+{
+	const DWORD buff_size = 1000;
+	LPWSTR buff = new TCHAR[buff_size];
+	GetEnvironmentVariableW(L"USERPROFILE", buff, buff_size);
+
+	wsprintfW(redirect, L"%s\\Documents\\%s", buff, PathFindFileNameW(path));
+	OutputDebugString(redirect);
+}
+
+HANDLE(WINAPI* oCreateFileW)(LPCWSTR, DWORD, DWORD, LPSECURITY_ATTRIBUTES, DWORD, DWORD, HANDLE) = CreateFileW;
+
+// Hooker for CreatFile
+HANDLE WINAPI hookedCreateFileW(LPCWSTR lpFileName, DWORD dwDesiredAccess, DWORD dwShareMode, LPSECURITY_ATTRIBUTES lpSecurityAttributes,
+	DWORD dwCreationDisposition, DWORD dwFlagsAndAttributes, HANDLE hTemplateFile)
+{
+	LPWSTR redirect = new TCHAR[1000];
+	GetRedirectPath(lpFileName, redirect);
+	return oCreateFileW(
+		redirect, dwDesiredAccess, dwShareMode, lpSecurityAttributes, dwCreationDisposition, dwFlagsAndAttributes, hTemplateFile);
 }
 
 BOOL APIENTRY DllMain( HMODULE hModule,
@@ -20,15 +49,17 @@ BOOL APIENTRY DllMain( HMODULE hModule,
     {
     case DLL_PROCESS_ATTACH:
 	{
+		OutputDebugString(L"Add detour hooker...");
 		DetourTransactionBegin();
 		DetourUpdateThread(GetCurrentThread());
 		DetourAttach(&(PVOID&)oMessageBoxW, hookedMessageBoxW);
-
+		DetourAttach(&(PVOID&)oCreateFileW, hookedCreateFileW);
 		LONG lError = DetourTransactionCommit();
 		if (lError != NO_ERROR) {
-			MessageBox(HWND_DESKTOP, L"Could not add detour", L"Detour Error", MB_OK);
+			MessageBox(HWND_DESKTOP, L"Unable to add detour hooker", L"Detour Error", MB_OK);
 			return FALSE;
 		}
+		OutputDebugString(L"Completed to add detour hooker...");
 	}
     case DLL_THREAD_ATTACH:
     case DLL_THREAD_DETACH:
@@ -37,4 +68,5 @@ BOOL APIENTRY DllMain( HMODULE hModule,
     }
     return TRUE;
 }
+
 
--- Tester/Form1.cs
+++ Tester/Form1.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Windows.Forms;
+using System.IO;
 namespace Tester
 {
     public partial class Form1 : Form
@@ -11,6 +12,7 @@ namespace Tester
 
         private void button1_Click(object sender, EventArgs e)
         {
+            File.WriteAllText("D:\\Wang\\Test.txt", "Hello world");
             MessageBox.Show("Hello world!");
         }
     }
